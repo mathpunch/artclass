1a3fdeacd330ce51a102d93e54496aafd7337a55
mathpunch Official <ozzycouts51@gmail.com>
2025-01-02 04:43:38 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,23 @@
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import million from "million/compiler";
-import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+// https here is necesary for some features to work, even if this is going to be behind an SSL-providing reverse proxy.
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const Corrosion = require('corrosion');
 
-export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: `${uvPath}/.`.replace(/\\/g, "/"),
-          dest: "uv"
-        },
-        {
-          src: "public/uv/uv.config.js",
-          dest: "uv"
-        }
-      ]
-    }),
-    million.vite({ mode: "preact" })
-  ],
-  server: {
-    headers: {
-      "X-Frame-Options": "SAMEORIGIN"
-    },
-    proxy: {
-      "/cdn": {
-        target: "https://cdn.radon.games",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cdn/, ""),
-        headers: {
-          referer: "https://cdn.radon.games"
-        }
-      },
-      "/api": {
-        target: "https://api.radon.games",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        headers: {
-          referer: "https://api.radon.games"
-        }
-      }
-    }
-  }
+// you are free to use self-signed certificates here, if you plan to route through an SSL-providing reverse proxy.
+const ssl = {
+    key: fs.readFileSync(path.join(__dirname, '/ssl.key')),
+    cert: fs.readFileSync(path.join(__dirname, '/ssl.cert')),
+};
+const server = https.createServer(ssl);
+const proxy = new Corrosion({
+    codec: 'xor', // apply basic xor encryption to url parameters in an effort to evade filters. Optional.
+    prefix: '/get/' // specify the endpoint (prefix). Optional.
 });
+
+proxy.bundleScripts();
+
+server.on('request', (request, response) => {
+    if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
+    response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(8443); // port other than 443 if it is needed by other software.
